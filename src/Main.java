8b4ba96fa22681fc6e1df3aa20d392480924c9a4
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-07-29 13:12:22 +0200

    completato

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] carBrands = {"FITA", "AUDI", "TOYOTA", "BMW", "HONDA"};
+        System.out.println(carBrands.length);
+        System.out.println(carBrands[2]);
+
+        int[] primeNumbers = new int[6];
+        primeNumbers[0] = 10;
+        primeNumbers[1] = 20;
+        primeNumbers[2] = 30;
+        primeNumbers[3] = 40;
+        primeNumbers[4] = 50;
+        primeNumbers[5] = 60;
+
+        System.out.println(java.util.Arrays.toString(primeNumbers));
+    }
+    }
+
+
+
