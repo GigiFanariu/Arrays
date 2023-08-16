b47eed30f3404c2bc7b910f715aafcae81edba25
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-08-17 00:29:42 +0200

    Corretto

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         System.out.println(carBrands[2]);
 
         int[] primeNumbers = new int[6];
-        primeNumbers[0] = 10;
-        primeNumbers[1] = 20;
-        primeNumbers[2] = 30;
-        primeNumbers[3] = 40;
-        primeNumbers[4] = 50;
-        primeNumbers[5] = 60;
+        primeNumbers[0] = 2;
+        primeNumbers[1] = 3;
+        primeNumbers[2] = 17;
+        primeNumbers[3] = 7;
+        primeNumbers[4] = 11;
+        primeNumbers[5] = 13;
 
         System.out.println(java.util.Arrays.toString(primeNumbers));
     }
